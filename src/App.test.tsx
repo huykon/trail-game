71b41c02995080710529691d5573823ed4adacb7
huykon225 <huykon225@gmail.com>
2021-09-21 16:50:15 +0700

    feat: initial project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { useState } from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const scenes = [
+    {
+        id: 0,
+        background_url: "mountain0.jpg",
+        hitzones: [{ x: "21%", y: "50%", goto: 1 }],
+    },
+    {
+        id: 1,
+        background_url: "mountain1.jpg",
+        hitzones: [
+            { x: "25%", y: "50%", goto: 2 },
+            { x: "60%", y: "81%", goto: 0 },
+        ],
+    },
+];
+
+test("renders learn react link", () => {
+    render(<App />);
+    /* const linkElement = screen.getByText(/Current scene can not loaded/i);
+    expect(linkElement).toBeInTheDocument(); */
+    // expect
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ScenesData from "./assets/scenes.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const scenesArray = (ScenesData && ScenesData.scenes) || [];
+    const [currentScene, setCurrentScene] = useState(
+        scenesArray && scenesArray.length ? scenesArray[0] : null
+    );
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    const onChangeScene = (sceneId: Number) => {
+        if (scenesArray.find((scene) => scene && scene.id === sceneId)) {
+            const foundScene =
+                scenesArray.find((scene) => scene && scene.id === sceneId) ||
+                null;
+
+            setImageLoaded(false);
+            setCurrentScene(foundScene);
+        }
+    };
+
+    const onScrollImage = (direction: String) => {
+        const scrollSection = document.getElementById("scroll-section");
+        if (scrollSection) {
+            if (direction === "right") {
+                scrollSection.scrollLeft += 100;
+            } else {
+                scrollSection.scrollLeft -= 100;
+            }
+        }
+    };
+
+    return (
+        <div className="App">
+            {currentScene ? (
+                <div className="scene-area">
+                    {currentScene.background_url ? (
+                        <div className="scroll-section" id="scroll-section">
+                            <div className="container-image">
+                                <img
+                                    src={
+                                        require(`./assets/images/${currentScene.background_url}`)
+                                            .default
+                                    }
+                                    alt="Current picture"
+                                    onLoad={() => setImageLoaded(true)}
+                                />
+                                {imageLoaded &&
+                                currentScene.hitzones &&
+                                currentScene.hitzones.length
+                                    ? currentScene.hitzones.map(
+                                          (hitZone, idx) => (
+                                              <button
+                                                  key={idx}
+                                                  style={{
+                                                      left: hitZone.x,
+                                                      top: hitZone.y,
+                                                  }}
+                                                  onClick={() =>
+                                                      onChangeScene(
+                                                          hitZone.goto
+                                                      )
+                                                  }
+                                              >
+                                                  ↑
+                                              </button>
+                                          )
+                                      )
+                                    : null}
+                            </div>
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                    {imageLoaded && (
+                        <React.Fragment>
+                            <button
+                                className="nav-control nav-left"
+                                onClick={() => onScrollImage("left")}
+                            >
+                                ∆
+                            </button>
+                            <button
+                                className="nav-control nav-right"
+                                onClick={() => onScrollImage("right")}
+                            >
+                                ∆
+                            </button>
+                        </React.Fragment>
+                    )}
+                </div>
+            ) : (
+                <div className="text-center">Current scene can not loaded</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
